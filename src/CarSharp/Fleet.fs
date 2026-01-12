3543bb6837e4ea7864eb2de1f6d3fef8a86514ec
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2026-01-12 11:40:07 +0100

    feat(fs): impl remove (Green)

--- src/CarSharp/Fleet.fs
+++ src/CarSharp/Fleet.fs
@@ -2,15 +2,16 @@ module CarSharp.Fleet
 
 type Car = unit
 type Fleet = Fleet of Car list
+type Error = string
 
 let emptyFleet = Fleet []
 
-
-
 let count (Fleet cars) = cars.Length
 
-
-
 let add (car: Car) (Fleet cars) =
-
     Fleet (car :: cars)
+
+let remove (car: Car) (Fleet cars) =
+    match List.tryFindIndex ((=) car) cars with
+    | Some index -> Ok (Fleet (List.removeAt index cars))
+    | None -> Result.Error "Car not found"
