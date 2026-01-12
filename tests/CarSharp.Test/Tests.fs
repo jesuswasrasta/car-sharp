edd09824e3a4e2fdf67336f3b4107d76c775ff74
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2026-01-12 11:35:27 +0100

    test(fs): property adding a car increments count (Red)

--- tests/CarSharp.Test/Tests.fs
+++ tests/CarSharp.Test/Tests.fs
@@ -3,7 +3,13 @@ module Tests
 open System
 open CarSharp.Fleet
 open Xunit
+open FsCheck.Xunit
 
 [<Fact>]
 let ``Empty fleet has zero count`` () =
     Assert.Equal(0, count emptyFleet)
+
+[<Property>]
+let ``Adding a car increments count`` (car: Car) (fleet: Fleet) =
+    let newFleet = add car fleet
+    count newFleet = count fleet + 1
