c375290689df8f7ef6703d588de76fcb07ae142d
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2026-01-12 10:35:02 +0100

    test(cs): add Add_Should_IncrementCount (Red)

--- tests/CarSharp.CS.Test/FleetTests.cs
+++ tests/CarSharp.CS.Test/FleetTests.cs
@@ -5,14 +5,29 @@ namespace CarSharp.CS.Test;
 
 public class FleetTests
 {
-    [Fact]
-    public void NewFleet_Should_BeEmpty()
-    {
-        // Arrange
-        var fleet = new Fleet();
-
-        // Act & Assert
-        // This will fail compilation until we add Count property
-        Assert.Equal(0, fleet.Count);
+        [Fact]
+        public void NewFleet_Should_BeEmpty()
+        {
+            // Arrange
+            var fleet = new Fleet();
+    
+            // Act & Assert
+            // This will fail compilation until we add Count property
+            Assert.Equal(0, fleet.Count);
+        }
+    
+        [Fact]
+        public void Add_Should_IncrementCount()
+        {
+            // Arrange
+            var fleet = new Fleet();
+            var car = new Car();
+    
+            // Act
+            fleet.Add(car);
+    
+            // Assert
+            Assert.Equal(1, fleet.Count);
+        }
     }
-}
+    
