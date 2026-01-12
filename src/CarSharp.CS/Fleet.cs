b3914ef1c326b30273765295832fd6bdab9066b2
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2026-01-12 10:46:00 +0100

    feat(cs): impl Add method (Green)

--- src/CarSharp.CS/Fleet.cs
+++ src/CarSharp.CS/Fleet.cs
@@ -6,5 +6,18 @@ public class Fleet
 {
     private readonly List<Car> _cars = new();
 
-    public int Count => _cars.Count;
-}
+        public int Count => _cars.Count;
+
+    
+
+        public void Add(Car car)
+
+        {
+
+            _cars.Add(car);
+
+        }
+
+    }
+
+    
