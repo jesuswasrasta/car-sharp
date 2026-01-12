5d7f7e4295a7fce35e0e33b59429e5798c852371
Ferdinando Santacroce <ferdinando.santacroce@gmail.com>
2026-01-12 11:39:39 +0100

    test(fs): add property removing a car previously added (Red)

--- tests/CarSharp.Test/Tests.fs
+++ tests/CarSharp.Test/Tests.fs
@@ -10,6 +10,25 @@ let ``Empty fleet has zero count`` () =
     Assert.Equal(0, count emptyFleet)
 
 [<Property>]
+
 let ``Adding a car increments count`` (car: Car) (fleet: Fleet) =
+
     let newFleet = add car fleet
+
     count newFleet = count fleet + 1
+
+
+
+[<Property>]
+
+let ``Removing a car previously added returns original count`` (car: Car) (fleet: Fleet) =
+
+    let newFleet = add car fleet
+
+    let result = remove car newFleet
+
+    match result with
+
+    | Ok f -> count f = count fleet
+
+    | Error _ -> false
